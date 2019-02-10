b115b615269062d384396a5973beab3a49960f9f
Elena <litishia@ya.ru>
2019-02-10 14:20:09 -0500

    final dysplay fixex

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,23 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component{
   state = { videos:[], selectedVideo: null };
 
+  componentDidMount(){
+    this.onTermChange('nirvana');
+  }
+
   onTermChange = async (term) => {
     let response = await youtube.get('/search', {
       params:{
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
-    //console.log(response.data.items);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+      });
   };
 
   onVideoSelect = (selectedVideo) => {
-    //console.log("fron the app!", selectedVideo);
     this.setState({selectedVideo: selectedVideo});
   };
 
@@ -26,11 +31,19 @@ class App extends React.Component{
     return (
       <div className= "ui container">
         <SearchBar onFormSumbit={this.onTermChange}/>
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo}/>
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
